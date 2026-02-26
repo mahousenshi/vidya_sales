33034546fd5f3e5a77e4fced77d255014e8e0117
Fabio Ortolan <mahousenshi@zoho.com>
2026-02-26 20:20:49 -0300

    Delete start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Rodar o script de seed
-echo "===> Rodando Seed de dados..."
-python seed.py
-
-# Iniciar o Uvicorn usando a porta definida pelo Render ou 8000 como padrão
-echo "===> Iniciando servidor FastAPI..."
-exec uvicorn app.main:app --host 0.0.0.0 --port ${PORT:-8000}
